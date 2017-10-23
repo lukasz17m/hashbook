1336f858e4f5adec9f924cb06f97ef3e5e64a42a
lukasz17m <lukasz17.m@gmail.com>
2017-10-23 22:18:25 +0200

    tagColor function improved using W3C YIQ algorithm

--- src/utils/tagColor.js
+++ src/utils/tagColor.js
@@ -9,18 +9,16 @@ import sr from 'seedrandom';
 const tagColor = tag => {
   const hex = Math.floor(sr(tag)() * 0x1000000).toString(16).padStart(6, '0');
 
-  const r = parseInt(hex.substr(0, 2), 16) / 255;
-  const g = parseInt(hex.substr(2, 2), 16) / 255;
-  const b = parseInt(hex.substr(4, 2), 16) / 255;
+  const r = parseInt(hex.substr(0, 2), 16);
+  const g = parseInt(hex.substr(2, 2), 16);
+  const b = parseInt(hex.substr(4, 2), 16);
 
-  const min = Math.min(r, g, b);
-  const max = Math.max(r, g, b);
-
-  const lightness = (max + min) / 2;
+  // Algorithm from W3C
+  const brightness = Math.round(((r * 299) + (g * 587) + (b * 114)) / 1000);
 
   return {
     background: `#${hex}`,
-    font: lightness > 0.5 ? '#000' : '#fff'
+    font: brightness > 125 ? '#000' : '#fff'
   };
 }
 
